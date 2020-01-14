b6af4d26ab3e6f6caa02aa0919bd1a7dd72c5e78
Sandor Martin Leyva <sandormartin37@gmail.com>
2020-01-13 21:02:40 -0500

    Added reducers without functionality

--- src/reducers/crud.js
+++ src/reducers/crud.js
@@ -1,18 +1,33 @@
+import {
+  ADD_PROPERTY,
+  UPDATE_PROPERTY,
+  DELETE_PROPERTY,
+  LOAD_PROPERTIES
+} from '../actions';
+
 let initialState = {
   properties: []
 };
 
 const crud = (state = initialState, action) => {
   switch (action.type) {
-    
-    case 'ADD_PROPERTY':
-      
+
+    case ADD_PROPERTY:
       return {
-        
+        ...state
       }
-
-
-
+    case UPDATE_PROPERTY:
+      return {
+        ...state
+      }
+    case DELETE_PROPERTY:
+      return {
+        ...state
+      }
+    case LOAD_PROPERTIES:
+      return {
+        ...state
+      }
     default:
       return state
   }
