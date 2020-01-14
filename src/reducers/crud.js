977f4b57345505a73cff7192c1b5d96dc6758103
Sandor Martin Leyva <sandormartin37@gmail.com>
2020-01-13 20:21:40 -0500

    Added empty reducer

---
+++ src/reducers/crud.js
@@ -0,0 +1,21 @@
+let initialState = {
+  properties: []
+};
+
+const crud = (state = initialState, action) => {
+  switch (action.type) {
+    
+    case 'ADD_PROPERTY':
+      
+      return {
+        
+      }
+
+
+
+    default:
+      return state
+  }
+}
+
+export default crud
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import crud from './crud'
+
+const reducer = combineReducers({
+  crud,
+})
+
+export default reducer
